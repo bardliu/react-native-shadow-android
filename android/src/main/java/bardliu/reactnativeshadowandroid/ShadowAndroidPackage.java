39d26021b195a08afe7517795ef8e052a5769c53
BardLiu <ibardliu@gmail.com>
2024-06-20 00:47:09 +0800

    fix: can't load shadow

--- android/src/main/java/bardliu/reactnativeshadowandroid/ShadowAndroidPackage.java
+++ android/src/main/java/bardliu/reactnativeshadowandroid/ShadowAndroidPackage.java
@@ -1,5 +1,7 @@
 package bardliu.reactnativeshadowandroid;
 
+import androidx.annotation.NonNull;
+
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -10,13 +12,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class ShadowAndroidPackage implements ReactPackage {
+    @NonNull
     @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
 
+    @NonNull
     @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Arrays.<ViewManager>asList(new ShadowAndroidViewManager());
+    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
+        return List.of(new ShadowViewManager(reactContext));
     }
 }
