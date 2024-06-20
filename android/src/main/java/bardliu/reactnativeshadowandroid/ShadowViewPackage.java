717d5d7f194eb90788abc16b69db7cb56a632d8a
BardLiu <bardliu@openrice.com>
2024-06-20 10:13:43 +0800

    chore: replace ShadowAndroidPackage to ShadowViewPackage

--- android/src/main/java/bardliu/reactnativeshadowandroid/ShadowViewPackage.java
+++ android/src/main/java/bardliu/reactnativeshadowandroid/ShadowViewPackage.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class ShadowAndroidPackage implements ReactPackage {
+public class ShadowViewPackage implements ReactPackage {
     @NonNull
     @Override
     public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
